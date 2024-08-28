133f56e4f0fb7069f62c333c0b66501e34efab4c
pvapes <efrenlopez.dev@outlook.com>
2024-08-27 22:42:05 -0500

    Correction of index.html

--- public/js/vapes.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    const images = document.querySelectorAll('.image-wrapper img');
-
-    images.forEach((img, index) => {
-        img.style.transform = `translateY(-${scrollPosition * (0.5 + index * 0.1)}px)`;
-    });
-});
